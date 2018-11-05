03d2fb6ef3c80fe8e8cf00702f27a2ef61e4a911
gongnanxiong <gongnanxiong235@126.com>
2018-11-05 17:39:36 +0800

    'hello'

---
+++ day5/formatInput.py
@@ -0,0 +1,11 @@
+name=input("Name:")
+age=int(input("Age:"))
+job=input("Job:")
+
+# print(age.isdigit())
+msg='''---------------info of gongnanxiong---------------
+Name:%s
+age:%d
+job:%s
+''' %(name,age,job)
+print(msg)
