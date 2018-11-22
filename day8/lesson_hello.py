d8407b014187372a67fe6e5bfca2823d3e900b7d
龚南熊 <18151143059@163.com>
2018-11-22 10:33:58 +0800

    hello

--- day8/lesson_hello.py
+++ day8/lesson_hello.py
@@ -2,4 +2,6 @@
 # date:2018/11/22
 
 with open('hello.txt','w',encoding='utf-8') as file_write:
-    file_write.writelines(['hello','world','gong','nan','xiong'])
+    lines=['hello','world','gong','nan','xiong']
+    lines=[x+'\n' for x in lines]
+    file_write.writelines(lines)
