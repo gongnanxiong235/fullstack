c11dc12677dc0421b62a3a9bc6051bac4ad3f11d
gongnanxiong235 <18151143059@163.com>
2018-12-16 12:28:51 +0800

    ddd

--- day33/database.py
+++ day33/database.py
@@ -6,13 +6,13 @@ import pymysql
 # from config import ConfigUtil as c
 from day33.config import ConfigUtil
 
-hp = ConfigUtil()
-host_name = hp.read_config_database('host')
-username_name = hp.read_config_database('username')
-password_name = hp.read_config_database('password')
-port_name = int(hp.read_config_database('port'))
-database_name = hp.read_config_database('database')
 class MysqldbHelper(object):
+    conf = ConfigUtil()
+    host_name = conf.read_config_database('host')
+    username_name = conf.read_config_database('username')
+    password_name = conf.read_config_database('password')
+    port_name = int(conf.read_config_database('port'))
+    database_name = conf.read_config_database('database')
     def __init__(self, host=host_name, username=username_name, password=password_name, port=port_name,
                  database=database_name):
         self.host = host
@@ -34,7 +34,7 @@ class MysqldbHelper(object):
         """关闭数据库连接
 
         """
-        if not self.con:
+        if  self.con:
             self.con.close()
         else:
             raise Exception("DataBase doesn't connect,close connectiong error;please check the db config.")
@@ -50,38 +50,31 @@ class MysqldbHelper(object):
     返回值：
     1.返回值是元祖:表明查询到了结果
     2.返回值是None：表明没有查询到结果
-    3.字符串：表明已经捕捉到了异常
-    4.rtype:返回类型，如果etype=dict 以字典的形式返回查询到的数据，相反则以二维元祖的形式返回数据
-    5.type=all  返回全部结果，type=one 只返回第一条结果
+    3.rtype:返回类型，如果etype=dict 以字典的形式返回查询到的数据，相反则以二维元祖的形式返回数据
+    4.type=all  返回全部结果，type=one 只返回第一条结果
     '''
 
     def executeSelect(self, sql='', args=(), type='all', rtype=''):
         # 执行sql语句，针对读操作返回结果集
+        records=""
         if rtype == 'dict':
             self.cur = self.con.cursor(cursor=pymysql.cursors.DictCursor)
-        try:
-            self.cur.execute(sql, args=args)
-            print('执行sql语句:', self.cur._last_executed)  # 打印sql语句
-            if type == 'one':
-                records = self.cur.fetchone()
-            else:
-                records = self.cur.fetchall()
-        except Exception as e:
-            print('e:', e)
-            records = 'MySQL execute failed! ERROR (%s): %s' % (e.args[0], e.args[1])
-            print('erro:', records)
-        return records
-
-
+        self.cur.execute(sql, args=args)
+        print('执行sql语句:', self.cur._last_executed)  # 打印sql语句
+        if type == 'one':
+            records = self.cur.fetchone()
+            print("records:", records)
+        else:
+            records = self.cur.fetchall()
+            print("records:",records)
+        return  records
 
 
     '''
     针对删改 等操作失败会自动回滚   
     返回值:
     1.None：表明已经执行成功
-    2.字符串：表明已经捕捉到了异常
     '''
-
     def executeUpDe(self, sql='', args=()):
         try:
             self.cur.execute(sql, args)
@@ -91,18 +84,34 @@ class MysqldbHelper(object):
             self.con.rollback()
             error = 'MySQL execute failed! ERROR (%s): %s' % (e.args[0], e.args[1])
             print("error:", error)
-            return error
+            raise Exception(error)
+
 
     '''
     针对insert 返回主键
     '''
     def executeInsert(self,sql='', args=()):
-        result=self.executeCommit(sql,args)
-        if result is None:
-            return self.cur.lastrowid
-        else:
-            return result
+        self.executeUpDe(sql,args)
+        return self.cur.lastrowid
+
 
 
 if __name__ == '__main__':
-    print(MysqldbHelper().host)
+    import random
+    import time
+    helper=MysqldbHelper()
+    '''97-122  65-90'''
+    a= list(range(97,123))+list(range(65,91))
+
+    begin=time.time()
+    for i in range(1000):
+        name=''
+        for i in range(8):
+            name+=chr(random.choice(a))
+        print(name)
+        age=random.randint(20,35)
+        gender=random.randint(1,2)
+        NUMPER=helper.executeInsert('insert into tb1 (name,age,gender) values(%s,%s,%s)',(name,age,gender))
+        print(NUMPER)
+    end=time.time()
+    print("耗时:%s秒" % (end-begin))
